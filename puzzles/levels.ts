d9ee53efd1a0e006dfeeaa4de57721784599d479
Tyler Auer <Tyler.Auer@gmail.com>
2020-12-01 10:56:18 -0800

    add outlines for levels 3 and 4

--- puzzles/levels.ts
+++ puzzles/levels.ts
@@ -6,15 +6,28 @@ export const levels: { [key: string]: Level } = {
     levelBelow: null,
     levelAbove: 2,
     puzIndexes: [],
-    desc:
-      'Quick puzzles using arithmetic operations that introduce the structure of the puzzles.',
+    desc: 'Quick puzzles using arithmetic operations and whole numbers.',
   },
   '2': {
     id: 2,
     levelBelow: 1,
     levelAbove: null,
     puzIndexes: [],
+    desc: 'Trickier puzzles with decimals and the arithmetic operations.',
+  },
+  '3': {
+    id: 3,
+    levelBelow: 2,
+    levelAbove: null,
+    puzIndexes: [],
     desc:
-      'A step up from level 1. These puzzles tend to take a little more thought and exploration.',
+      'Level 3 puzzle introduce blocks -- numbers you must avoid as you find your way to the target',
+  },
+  '4': {
+    id: 4,
+    levelBelow: 3,
+    levelAbove: null,
+    puzIndexes: [],
+    desc: 'Level 4 puzzles add new operations.',
   },
 };
