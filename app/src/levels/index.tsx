75bb9b09735c82bd963cdc544193194ff4932b28
Tyler Auer <Tyler.Auer@gmail.com>
2020-12-04 14:23:59 -0800

    move level fetch to recoil selector

--- app/src/levels/index.tsx
+++ app/src/levels/index.tsx
@@ -1,8 +1,8 @@
+import { Suspense } from 'react';
 import {useParams, useHistory} from 'react-router-dom';
-import {useRecoilValue} from 'recoil';
-import {level} from '../state/level';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {levelData, levelId} from '../state/level';
 import {userInfo} from '../state/user';
-import useLoadLevel from '../hooks/useLoadLevel'
 
 import './index.scss'
 
@@ -12,16 +12,23 @@ interface Params {
 
 
 export default function LevelPage() {
-  const lvl = useRecoilValue(level)
+  return (
+    <Suspense fallback={' '}>
+      <LevelDisplay/>
+    </Suspense>
+  )
+}
+
+function LevelDisplay() {
+  const [lvlId, setLvlId] = useRecoilState(levelId)
+  const lvl = useRecoilValue(levelData)
   const {progress} = useRecoilValue(userInfo)
   const {level_id} = useParams<Params>() 
   const history = useHistory()
   
-  // Load level if needed
-  useLoadLevel(level_id)
-
-  // Don't rended HTML until level info is loaded
-  if (!lvl || level_id !== lvl.id.toString()) return null
+  if (lvlId !== parseInt(level_id)) {
+    setLvlId(parseInt(level_id))
+  }
 
   const onPuzClick = (id: number): void => {
     history.push(`/puzzle/${id}`)
@@ -31,7 +38,7 @@ export default function LevelPage() {
     history.push(`/level/${id}`)
   }
 
-  // Make stars that site below puzzle buttons
+  // Make stars that sit below puzzle buttons
   const puzList = lvl.puzIndexes.map(({id, stars}, i) => {
     const starsEarnedByUser = progress[id]?.filter(i => i).length || 0;
     const starsForPuz = stars.length;
@@ -49,14 +56,14 @@ export default function LevelPage() {
     }
 
     return (
-      <div key={i} className='lvl-page__puz-and-star-wrapper'>
-        <button 
-          className='lvl-page__puzzle-btn' 
-          onClick={() => onPuzClick(id)}>
-            {i+1}
-        </button>
-        <div className='lvl-page__stars'>{starList}</div>
-      </div>
+        <div key={i} className='lvl-page__puz-and-star-wrapper'>
+          <button 
+            className='lvl-page__puzzle-btn' 
+            onClick={() => onPuzClick(id)}>
+              {i+1}
+          </button>
+          <div className='lvl-page__stars'>{starList}</div>
+        </div>
     )
   });
 
@@ -69,25 +76,23 @@ export default function LevelPage() {
       <div className="lvl-page__list-of-puzzles">
         {puzList}
       </div>
-      <div className="lvl-page__nav">
-        <div className="lvl-page__left">
+        <div className="lvl-page__nav">
+          <div className="lvl-page__left">
             {lvl.levelBelow && (
-              <button className='lvl-page__level-btn' onClick={() => onLvlClick(lvl.levelBelow as number)}>
-                Level {lvl.levelBelow}
+             <button className='lvl-page__level-btn' onClick={() => onLvlClick(lvl.levelBelow as number)}>
+               Level {lvl.levelBelow}
               </button>
             )}
+          </div>
+          <div className="lvl-page__mid"></div>
+          <div className="lvl-page__right">
+              {lvl.levelAbove && (
+                <button className='lvl-page__level-btn' onClick={() => onLvlClick(lvl.levelAbove as number)}>
+                  Level {lvl.levelAbove}
+                </button>
+              )}
+          </div>
         </div>
-        <div className="lvl-page__mid">
-          
-        </div>
-        <div className="lvl-page__right">
-            {lvl.levelAbove && (
-              <button className='lvl-page__level-btn' onClick={() => onLvlClick(lvl.levelAbove as number)}>
-                Level {lvl.levelAbove}
-              </button>
-            )}
-        </div>
-      </div>
-    </div>
+    </div>  
   )
 }
--- app/src/state/level.ts
+++ app/src/state/level.ts
@@ -1,13 +1,25 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { Level } from '../structs/level';
 
 // SHAPES //////////////////////////////////////////////////////////////////////
 
 // ATOMS ///////////////////////////////////////////////////////////////////////
 
-export const level = atom({
-  key: 'level',
-  default: null as Level | null,
+export const levelId = atom({
+  key: 'levelId',
+  default: 1,
 });
 
 // SELECTORS ///////////////////////////////////////////////////////////////////
+
+/**
+ * Loads level data from the API based on the levelID
+ */
+export const levelData = selector({
+  key: 'levelData',
+  get: async ({ get }) => {
+    const res = await fetch(`/level/${get(levelId)}`);
+
+    return (await res.json()) as Level;
+  },
+});
