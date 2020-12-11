a2435b4c3f7c226de2ea67739cb93d865d2e4f88
Tyler Auer <Tyler.Auer@gmail.com>
2020-12-11 11:24:29 -0800

    add puzzle 2-6

--- puzzles/findSolutions.ts
+++ puzzles/findSolutions.ts
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import { puzzle } from './1-9';
+import { puzzle } from './2-6';
 import genOpBtnTextAndOp from '../app/src/utils/genOpBtnTextAndOp';
 
 interface Solution {
