7a3c67e7f2c216ddddbd142c54507c3b35d135d9
Tyler Auer <Tyler.Auer@gmail.com>
2020-12-12 10:55:13 -0800

    add puzzle 1-10

---
+++ puzzles/1-10.ts
@@ -0,0 +1,41 @@
+import { OpType, Puzzle } from '../app/src/structs/puzzle';
+
+export const puzzle: Puzzle = {
+  id: 23,
+  level: 1,
+  indexInLevel: 10,
+  disabled: false,
+  creator: 'Tyler',
+  start: 4,
+  target: 11,
+  maxMoves: null,
+  operations: [
+    {
+      symbol: OpType.sub,
+      value: 1,
+    },
+    {
+      symbol: OpType.mult,
+      value: 11,
+    },
+    {
+      symbol: OpType.div,
+      value: 2,
+    },
+  ],
+  blocks: [],
+  stars: [
+    {
+      moves: 3,
+      goalRelation: 'exactly',
+    },
+    {
+      moves: 4,
+      goalRelation: 'exactly',
+    },
+    {
+      moves: 8,
+      goalRelation: 'more',
+    },
+  ],
+};
