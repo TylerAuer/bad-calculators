5339307865ff2fca1a08fa91a4ab2d7867d17a15
Tyler Auer <Tyler.Auer@gmail.com>
2021-01-02 11:18:30 -0800

    remove test cron job

--- server/init/startCronJobs.ts
+++ server/init/startCronJobs.ts
@@ -4,7 +4,4 @@ import emailSiteSummary from '../summary/emailSiteSummary';
 export default async function startCronJobs() {
   const siteSummaryEmailJob = new CronJob('1 0 * * *', emailSiteSummary);
   siteSummaryEmailJob.start();
-
-  const rapidEmailCron = new CronJob('* * * * *', emailSiteSummary);
-  rapidEmailCron.start();
 }
