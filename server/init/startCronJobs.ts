2d129efe7a4f350ec181da21ba1f53e07d6f365d
Tyler Auer <Tyler.Auer@gmail.com>
2021-01-02 17:38:10 -0800

    add timezone offset to cron job

--- server/init/startCronJobs.ts
+++ server/init/startCronJobs.ts
@@ -2,6 +2,13 @@ import { CronJob } from 'cron';
 import emailSiteSummary from '../summary/emailSiteSummary';
 
 export default async function startCronJobs() {
-  const siteSummaryEmailJob = new CronJob('1 0 * * *', emailSiteSummary);
+  const siteSummaryEmailJob = new CronJob(
+    '1 0 * * *',
+    emailSiteSummary,
+    null,
+    undefined,
+    'America/Los_Angeles'
+  );
+
   siteSummaryEmailJob.start();
 }
