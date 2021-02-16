f1a8dfd38ddf5aa6b2e47c8a2bbc6852e52e12a5
Tyler Auer <Tyler.Auer@gmail.com>
2021-02-15 22:37:26 -0800

    create look and say puzzle

---
+++ puzzles/4-7.ts
@@ -0,0 +1,37 @@
+import { OpType, Puzzle } from '../app/src/structs/puzzle';
+
+export const puzzle: Puzzle = {
+  id: 33,
+  level: 4,
+  indexInLevel: 7,
+  disabled: false,
+  creator: 'Tyler',
+  start: 3,
+  target: 25,
+  maxMoves: null,
+  operations: [
+    {
+      symbol: OpType.add,
+      value: 3,
+    },
+    {
+      symbol: OpType.look_and_say,
+      limit: 2,
+    },
+  ],
+  blocks: [],
+  stars: [
+    {
+      moves: 5,
+      goalRelation: 'exactly',
+    },
+    {
+      moves: 6,
+      goalRelation: 'exactly',
+    },
+    {
+      moves: 6,
+      goalRelation: 'more',
+    },
+  ],
+};
