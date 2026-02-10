caff2fdf32efdcf848b3122aa6f551118b0cb744
Tyler Auer <32402444+TylerAuer@users.noreply.github.com>
2026-02-10 16:48:54 -0700

    switch to sass instead of node-sass (#70)
    * switch to sass instead of node-sass
    
    * update more

--- app/src/Spinner/index.tsx
+++ app/src/Spinner/index.tsx
@@ -1,10 +1,10 @@
-import Loader from 'react-loader-spinner';
+import { Oval } from 'react-loader-spinner';
 import './index.scss';
 
 export default function Spinner() {
   return (
     <div className="spinner">
-      <Loader type="Oval" height={75} width={75} />
+      <Oval height={75} width={75} />
     </div>
   );
 }
